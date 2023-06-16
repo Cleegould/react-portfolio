f203490c9a10d7bfdd59c0819fc2baadd4783507
chrisg <cleegould@gmail.com>
2023-06-15 21:35:33 -0400

    added correct pictures of projects and linked them, finshed footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,8 +3,8 @@ import myPic from "/Users/christopher/Desktop/20HW/react-portfolio/src/assets/IM
 import myBackground from '/Users/christopher/Desktop/20HW/react-portfolio/src/assets/modern-banner-with-abstract-low-poly-design_1048-14340.avif';
 import project1Image from '/Users/christopher/Desktop/20HW/react-portfolio/src/assets/Screenshot 2023-03-14 at 5.12.43 PM.png';
 import project2Image from '/Users/christopher/Desktop/20HW/react-portfolio/src/assets/Screenshot 2023-05-15 at 6.16.30 PM.png';
-import project3Image from '/Users/christopher/Desktop/20HW/react-portfolio/src/assets/Screenshot 2023-05-15 at 6.16.30 PM.png';
-import project4Image from '/Users/christopher/Desktop/20HW/react-portfolio/src/assets/Screenshot 2023-05-15 at 6.16.30 PM.png';
+import project3Image from '/Users/christopher/Desktop/20HW/react-portfolio/src/assets/Screenshot 2023-06-15 at 9.28.33 PM.png';
+import project4Image from '/Users/christopher/Desktop/20HW/react-portfolio/src/assets/Screenshot 2023-06-15 at 9.33.07 PM.png';
 
 
 export default function App() {
@@ -21,8 +21,8 @@ export default function App() {
   };
 
   const projectImageStyle = {
-    width: '400px', // Adjust the width to your desired size
-    height: '200px', // Adjust the height to your desired size
+    width: '400px', 
+    height: '200px', 
   };
 
   return (
@@ -44,16 +44,16 @@ export default function App() {
       <div id="projects">
         <h1>Projects</h1>
         <div className="project-images">
-          <a href="https://www.example1.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://trengland.github.io/MoveMe-Scene/" target="_blank" rel="noopener noreferrer">
             <img src={project1Image} alt="Project 1" style={projectImageStyle} />
           </a>
-          <a href="https://www.example2.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/ALTaylor33/grilled-meats-and-treats" target="_blank" rel="noopener noreferrer">
             <img src={project2Image} alt="Project 2" style={projectImageStyle} />
           </a>
-          <a href="https://www.example3.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://sleepy-falls-23209.herokuapp.com/" target="_blank" rel="noopener noreferrer">
             <img src={project3Image} alt="Project 3" style={projectImageStyle} />
           </a>
-          <a href="https://www.example4.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://texteditorpwahw19.herokuapp.com/" target="_blank" rel="noopener noreferrer">
             <img src={project4Image} alt="Project 4" style={projectImageStyle} />
           </a>
         </div>
